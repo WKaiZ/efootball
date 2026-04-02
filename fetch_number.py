9ba82b4dcad1f6c232f41e936fa4c70be44373d5
Wesley Zheng <wzheng0302@berkeley.edu>
2026-04-02 11:24:48 -0700

    adjustment to ratings

--- fetch_number.py
+++ fetch_number.py
@@ -104,7 +104,38 @@ async def fetch_transfermarkt_rueckennummern_html(playwright, url: str):
     if curl_html:
         return curl_html
     return await _transfermarkt_rueckennummern_playwright(playwright, url)
-MANUAL_ID_OVERRIDES = {'argentina': {'nico gonzalez': {'player_id': '486031', 'preserve_name': True}}, 'brazil': {'gabriel': {'player_id': '435338', 'preserve_name': True}, 'ederson': {'player_id': '607854', 'preserve_name': True}, 'vitinho': {'player_id': '468249', 'preserve_name': True}, 'pepe': {'player_id': '520662', 'preserve_name': True}, 'pedro': {'player_id': '432895', 'preserve_name': True}, 'allan': {'player_id': '126422', 'preserve_name': True}, 'oscar': {'player_id': '85314', 'preserve_name': True}, 'leo pereira': {'player_id': '288431', 'preserve_name': True}, 'igor thiago': {'player_id': '739443', 'preserve_name': True}, 'joao gomes': {'player_id': '735570', 'preserve_name': True}, 'gerson': {'player_id': '341705', 'preserve_name': True}, 'fred': {'player_id': '191614', 'preserve_name': True}, 'douglas luiz': {'player_id': '447661', 'preserve_name': True}}, 'portugal': {'pepe': {'player_id': '14132', 'preserve_name': True}}, 'france': {'maxence lacroix': {'player_id': '434224', 'preserve_name': True}}, 'spain': {'pedro': {'player_id': '65278', 'preserve_name': True}}}
+MANUAL_ID_OVERRIDES = {
+    'argentina': {
+        'nico gonzalez': {'player_id': '486031', 'preserve_name': True}
+    },
+    'brazil': {
+        'gabriel': {'player_id': '435338', 'preserve_name': True},
+        'ederson': {'player_id': '607854', 'preserve_name': True},
+        'vitinho': {'player_id': '468249', 'preserve_name': True},
+        'pepe': {'player_id': '520662', 'preserve_name': True},
+        'pedro': {'player_id': '432895', 'preserve_name': True},
+        'allan': {'player_id': '126422', 'preserve_name': True},
+        'oscar': {'player_id': '85314', 'preserve_name': True},
+        'leo pereira': {'player_id': '288431', 'preserve_name': True},
+        'igor thiago': {'player_id': '739443', 'preserve_name': True},
+        'joao gomes': {'player_id': '735570', 'preserve_name': True},
+        'gerson': {'player_id': '341705', 'preserve_name': True},
+        'fred': {'player_id': '191614', 'preserve_name': True},
+        'douglas luiz': {'player_id': '447661', 'preserve_name': True}
+    },
+    'portugal': {
+        'pepe': {'player_id': '14132', 'preserve_name': True}
+    },
+    'france': {
+        'maxence lacroix': {'player_id': '434224', 'preserve_name': True}
+    },
+    'spain': {
+        'pedro': {'player_id': '65278', 'preserve_name': True}
+    }, 
+    'italy': {
+        'luca pellegrini': {'player_id': '346567', 'preserve_name': True}
+    }
+}
 
 def normalize_name(name):
     nfkd = unicodedata.normalize('NFKD', name)
